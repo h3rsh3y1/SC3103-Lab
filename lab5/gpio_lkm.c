4f60dc16fb0f996d955d84cc1f4b0933c69f52e0
h3rsh3y1 <harshalghate04@gmail.com>
2025-11-09 21:47:36 +0800

    corrected gpio_lkm.c

--- lab5/gpio_lkm.c
+++ lab5/gpio_lkm.c
@@ -1,70 +1,71 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <sys/stat.h>
-#include <sys/types.h>
-#include <fcntl.h>
-#include <sys/ioctl.h>
-#include <string.h>
-#include <unistd.h>
 #include <linux/gpio.h>
+#include <linux/init.h>
+#include <linux/interrupt.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
 
-/* you may want to look at the contents of this file, to understand
- the various elements of the data structure shown in the code below */
+static unsigned int ledGreen = 4;       // GPIO 4 (pin 7) connected to green LED
+static unsigned int pushButton = 11;    // GPIO 11 (pin 23) connected to button
+static unsigned int irqNumber;          // share IRQ num within file
+static bool ledOn = false;                  // toggle LED
 
-int main(int argc, char *argv[])
-{ 
-    int fd0 = open("/dev/gpiochip0",O_RDWR); // open the file descriptor
-    struct gpiochip_info cinfo;
-    ioctl(fd0,GPIO_GET_CHIPINFO_IOCTL,&cinfo); // get the chip information
-    fprintf(stdout, "GPIO chip 0: %s, \"%s\", %u lines\n", 
-        cinfo.name, cinfo.label, cinfo.lines);
+// GPIO IRQ handler function
+static irq_handler_t rpi_gpio_isr(unsigned int irq,void *dev_id, struct pt_regs *regs
+)
+{
+    ledOn = !ledOn;
+    gpio_set_value(ledGreen, ledOn);
 
-    struct gpiohandle_request req_pb; // push button
-    struct gpiohandle_data event;
-    struct gpiohandle_request req_GY; // Green and Yellow
-    struct gpiohandle_data data_GY; // for data bit
+    printk(KERN_ALERT "GPIO Interrupt!\n");
+    return (irq_handler_t) IRQ_HANDLED;
+}
 
-    req_pb.lines = 1; //pin 11 push button
-    req_pb.lineoffsets[0] = 11; //pin 11 push button
+// LKM exit function
+static void __exit rpi_gpio_exit(void)
+{
+    gpio_set_value(ledGreen, false);    // turn LED off
+    gpio_free(ledGreen);
+    gpio_free(pushButton);
+    free_irq(irqNumber, NULL);
 
-    req_pb.flags = GPIOHANDLE_REQUEST_INPUT;
+    printk(KERN_ALERT "Goodbye from the GPIO LKM\n");
+}
 
-    req_GY.lines = 3; // 1 pins in this handler    x
-    req_GY.lineoffsets[0] = 4; //pin 4 - Green LED
-    req_GY.lineoffsets[1] = 17; //pin 17 - Yellow LED
-    req_GY.lineoffsets[2] = 27; //pin 27 - Red LED
+// LKM initialisation function
+static int __init rpi_gpio_init(void)
+{
+    int result = 0;
+    printk(KERN_ALERT "Initialising the GPIO LKM\n");
 
-    req_GY.flags = GPIOHANDLE_REQUEST_OUTPUT; // set them to be output
+    ledOn = true;                           // default: LED on
+    gpio_request(ledGreen, "sysfs");        // request for LED GPIO
+    gpio_direction_output(ledGreen, ledOn); // set mode to output, turn LED on
 
-    ioctl(fd0, GPIO_GET_LINEHANDLE_IOCTL, &req_GY); // now get the line handler req_GY
-    ioctl(fd0, GPIO_GET_LINEHANDLE_IOCTL, &req_pb); // now get the line handler req_GY
-
-    data_GY.values[0] = 1; // set initial value of Green LED to High (ON)
-    data_GY.values[1] = 0; // set initial value of Yellow LED to Low (Off)
-    data_GY.values[2] = 0; // set initial value of Red LED to Low (Off)
-    ioctl(req_GY.fd, GPIOHANDLE_SET_LINE_VALUES_IOCTL, &data_GY); // output data bits
+    gpio_request(pushButton, "sysfs");      // request for button GPIO
+    gpio_direction_input(pushButton);       // set mode to input
+    int r = gpio_set_debounce(pushButton, 1000);    // debounce delay of 1000
+    printk(KERN_ALERT "return from debounce: %d\n", r);
     
-    usleep(1000000);
 
-    while(1){
-        ioctl(fd0, GPIO_GET_LINEHANDLE_IOCTL, &req_pb); // now get the line handler req_GY
-        ioctl(req_pb.fd, GPIOHANDLE_GET_LINE_VALUES_IOCTL, &event); // output data bits
-        ioctl(fd0, GPIO_GET_LINEHANDLE_IOCTL, &req_GY); // now get the line handler req_GY 
-        if(event.values[0]==1){
-            data_GY.values[2] = 1; // toggle
-        }else{
-            data_GY.values[2] = 0; // toggle
-        }
-        data_GY.values[0] = !data_GY.values[0]; // toggle
-        data_GY.values[1] = !data_GY.values[1]; // toggle
-        ioctl(req_GY.fd, GPIOHANDLE_SET_LINE_VALUES_IOCTL, &data_GY); // output data bits
-        usleep(100000); //sleep for 1 second
-        data_GY.values[2] = 0; // toggle
-        ioctl(req_GY.fd, GPIOHANDLE_SET_LINE_VALUES_IOCTL, &data_GY); // output data bits
-    }
-    close(req_pb.fd); // release line
-    close(req_GY.fd); // release line
-    close(fd0); // close the file
-    exit(EXIT_SUCCESS);
+    irqNumber = gpio_to_irq(pushButton);    // map button to IRQ num
+    printk(KERN_ALERT "Button mapped to IRQ: %d\n", irqNumber);
 
-}//main 
+    // request for interrupt line
+    result = request_irq(
+        irqNumber,
+        (irq_handler_t) rpi_gpio_isr,
+        IRQF_TRIGGER_RISING,
+        "rpi_gpio_handler",
+        NULL
+    );
+
+    return result;
+}
+
+module_init(rpi_gpio_init);
+module_exit(rpi_gpio_exit);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Harshal");
+MODULE_DESCRIPTION("Toggle Green LED with button");
+MODULE_VERSION("v1");
